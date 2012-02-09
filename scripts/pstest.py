8e1327cbfefbb6f90be3490560c685d55f194d20
Richard Shaw <jrs65@cita.utoronto.ca>
2012-02-09 15:10:52 -0500

    * Ue-Li scripts.

--- scripts/pstest.py
+++ scripts/pstest.py
@@ -9,22 +9,30 @@ from scipy.integrate import quad
 
 import numpy as np
 
-bt = beamtransfer.BeamTransfer("/mnt/scratch-3week/jrs65/chimebt/fullbt_50")
+import os
+import time
 
-klt = kltransform.KLTransform(bt, evsubdir='ev2/')
+mi = -200
+
+#bt = beamtransfer.BeamTransfer("/mnt/scratch-3week/jrs65/chimebt/fullbt_50")
+teldir = ((os.environ['SCRATCH'] if 'SCRATCH' in os.environ else ".") + ('/cylinder/chimebt50/band_4/'))
+bt = beamtransfer.BeamTransfer(teldir)
+
+
+klt = kltransform.KLTransform(bt)
 
 pse = psestimation.PSEstimation(klt)
 pse.bands = np.logspace(-3.0, 1.0, 4)
 pse.genbands()
 
 st = time.time()
-pk1 = klt.project_sky_matrix_forward(-50, pse.clarray[1])
+pk1 = klt.project_sky_matrix_forward(mi, pse.clarray[1])
 et = time.time()
 print (et - st)
 
 
 st = time.time()
-pk2 = klt.project_sky_matrix_forward_old(-50, pse.clarray[1])
+pk2 = klt.project_sky_matrix_forward_old(mi, pse.clarray[1])
 et = time.time()
 print (et - st)
 

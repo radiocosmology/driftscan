573d9505dde950cd6508425cfb5809560da37b22
Richard Shaw <jrs65@cita.utoronto.ca>
2012-05-28 11:05:34 -0400

    * Added a rudimentary averaging over the frequency window function.

--- cylsim/skymodel.py
+++ cylsim/skymodel.py
@@ -7,6 +7,33 @@ _cr = None
 
 _reionisation = False
 
+_zaverage = 5
+
+
+def clarray(aps, lmax, zarray, zaverage=None):
+
+    if zaverage == None:
+        zaverage = _zaverage
+
+    if zaverage == 1:
+        return aps(np.arange(lmax+1)[:, np.newaxis, np.newaxis],
+                   zarray[np.newaxis, :, np.newaxis], zarray[np.newaxis, np.newaxis, :])
+    else:
+        zhalf = np.abs(zarray[1] - zarray[0]) / 2.0
+        zlen = zarray.size
+        za = (zarray[:, np.newaxis] + np.linspace(-zhalf, zhalf, zaverage)[np.newaxis, :]).flatten()
+
+        lsections = np.array_split(np.arange(lmax+1), lmax / 50)
+
+        cla = np.zeros((lmax+1, zlen, zlen), dtype=np.float64)
+
+        for lsec in lsections:
+            clt = aps(lsec[:, np.newaxis, np.newaxis],
+                      za[np.newaxis, :, np.newaxis], za[np.newaxis, np.newaxis, :])
+
+            cla[lsec] = clt.reshape((-1, zlen, zaverage, zlen, zaverage)).sum(axis=4).sum(axis=2) / zaverage**2
+
+        return cla
 
 
 class PointSources(foregroundsck.PointSources):
@@ -31,7 +58,7 @@ def foreground_model(lmax, frequencies, npol):
         cv_fg[1, 1] = 0.3**2 * cv_fg[0, 0]
         cv_fg[2, 2] = 0.3**2 * cv_fg[0, 0]
 
-    cv_fg[0, 0] += skysim.clarray(fps.angular_powerspectrum, lmax, frequencies)
+    cv_fg[0, 0] += clarray(fps.angular_powerspectrum, lmax, frequencies)
     return cv_fg
 
 
@@ -47,9 +74,11 @@ def im21cm_model(lmax, frequencies, npol, cr = None):
             _cr = corr21cm.Corr21cm()
         cr = _cr
 
+    cr._freq_window = np.abs(cr.cosmology.comoving_distance(frequencies[0]) - cr.cosmology.comoving_distance(frequencies[1]))
+
     cv_sg = np.zeros((npol, npol, lmax+1, nfreq, nfreq))
 
-    cv_sg[0, 0] = skysim.clarray(cr.angular_powerspectrum, lmax, frequencies)
+    cv_sg[0, 0] = clarray(cr.angular_powerspectrum, lmax, frequencies)
 
     if _reionisation:
         cv_sg[0, 0] = 1e5 * cv_sg[0, 0]

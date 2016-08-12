d8fd1e17d8062aed13e51d1225f096d76af09ef2
Richard Shaw <richard@phas.ubc.ca>
2016-08-12 15:49:21 -0700

    Changed TransitTelescope to be a sublass of caput.time.Observer. This means that the location of the telescope is now set via latitude and longitude properties.
    Added a local_origin property.

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 from setuptools import setup, find_packages
 
-setup(
-    name = 'driftscan',
-    version = 0.1,
+import drift
 
-    packages = find_packages(),
-    requires = ['numpy', 'scipy', 'healpy', 'h5py', 'caput', 'cora'],
-    package_data = {'drift.telescope' : ['gmrtpositions.dat'] },
-    scripts = ['scripts/drift-makeproducts', 'scripts/drift-runpipeline'],
+setup(
+    name='driftscan',
+    version=drift.__version__,
+
+    packages=find_packages(),
+    requires=['numpy', 'scipy', 'healpy', 'h5py', 'caput>=0.3.0', 'cora'],
+    package_data={'drift.telescope': ['gmrtpositions.dat'] },
+    scripts=['scripts/drift-makeproducts', 'scripts/drift-runpipeline'],
 
     # metadata for upload to PyPI
-    author = "J. Richard Shaw",
-    author_email = "jrs65@cita.utoronto.ca",
-    description = "Transit telescope analysis with the m-mode formalism",
-    license = "GPL v3.0",
-    url = "http://github.com/CITA/driftscan"
+    author="J. Richard Shaw",
+    author_email="richard@phas.ubc.ca",
+    description="Transit telescope analysis with the m-mode formalism",
+    license="GPL v3.0",
+    url="http://github.com/radiocosmology/driftscan"
 )

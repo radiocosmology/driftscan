7ac7603be0bf24c4519f0d23960b761481a81c48
Richard Shaw <richard@phas.ubc.ca>
2019-05-03 17:37:51 -0400

    test: update test suite to use pytest
    A large change to the test suite to use pytest and to add the test data
    directly to the repository using git-lfs. This avoids the need to
    separately download the data with the fetch/push scripts (now removed).

--- tests/pushproducts.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Zip up test products
-tar czf drift_testproducts.tar.gz saved_products
-scp drift_testproducts.tar.gz jrs65@prawn.cita.utoronto.ca:public_html/
--- tests/updateproducts.sh
+++ tests/updateproducts.sh
@@ -3,18 +3,27 @@
 ## Copy over the products we wil test against. Only use when updating the
 ## products after verifying that they are correct.
 
+TEMP_PROD_DIR=$(mktemp -d -t tempprod)
+
 # Copy over beam properties
-cp tmptestdir/testdir/bt/beam_f/2/beam.hdf5 saved_products/beam_f_2.hdf5
-cp tmptestdir/testdir/bt/beam_m/14/beam.hdf5 saved_products/beam_m_14.hdf5
-cp tmptestdir/testdir/bt/beam_m/14/svd.hdf5 saved_products/svd_m_14.hdf5
-cp tmptestdir/testdir/bt/svdspectrum.hdf5 saved_products/svdspectrum.hdf5
+cp tmptestdir/testdir/bt/beam_m/14/beam.hdf5 $TEMP_PROD_DIR/beam_m_14.hdf5
+cp tmptestdir/testdir/bt/beam_m/14/svd.hdf5 $TEMP_PROD_DIR/svd_m_14.hdf5
+cp tmptestdir/testdir/bt/svdspectrum.hdf5 $TEMP_PROD_DIR/svdspectrum.hdf5
 
 # Copy over eigenvalue products
-cp tmptestdir/testdir/bt/dk/ev_m_33.hdf5 saved_products/ev_dk_m_33.hdf5
-cp tmptestdir/testdir/bt/kl/ev_m_26.hdf5 saved_products/ev_kl_m_26.hdf5
-cp tmptestdir/testdir/bt/dk/evals.hdf5 saved_products/evals_dk.hdf5
-cp tmptestdir/testdir/bt/kl/evals.hdf5 saved_products/evals_kl.hdf5
+cp tmptestdir/testdir/bt/dk/ev_m_38.hdf5 $TEMP_PROD_DIR/ev_dk_m_38.hdf5
+cp tmptestdir/testdir/bt/kl/ev_m_26.hdf5 $TEMP_PROD_DIR/ev_kl_m_26.hdf5
+cp tmptestdir/testdir/bt/dk/evals.hdf5 $TEMP_PROD_DIR/evals_dk.hdf5
+cp tmptestdir/testdir/bt/kl/evals.hdf5 $TEMP_PROD_DIR/evals_kl.hdf5
 
 # Copy Fisher matrices
-cp tmptestdir/testdir/bt/dk/ps2/fisher.hdf5 saved_products/fisher_dk.hdf5
-cp tmptestdir/testdir/bt/kl/ps1/fisher.hdf5 saved_products/fisher_kl.hdf5
+cp tmptestdir/testdir/bt/dk/ps2/fisher.hdf5 $TEMP_PROD_DIR/fisher_dk.hdf5
+cp tmptestdir/testdir/bt/kl/ps1/fisher.hdf5 $TEMP_PROD_DIR/fisher_kl.hdf5
+
+# Create the tarball
+cd $TEMP_PROD_DIR
+tar czf drift_testproducts.tar.gz *
+cd ~-
+mv $TEMP_PROD_DIR/drift_testproducts.tar.gz .
+
+rm -r $TEMP_PROD_DIR

17ffdd313e9e4e84356e2c3d06331ea2dbde3e5f
Carolin Hofer <cahofer@cedar1.cedar.computecanada.ca>
2019-10-23 17:38:17 -0700

    feat(beamtransfer): option to skip inverting svd beamtranfers
    (cherry picked from commit 49e97c74a5c4606a9eddbc80ef95b0d5d09f47b9)

--- drift/core/manager.py
+++ drift/core/manager.py
@@ -91,6 +91,7 @@ class ProductManager(object):
     gen_proj = False
 
     skip_svd = False
+    skip_svd_inv = False
 
     @classmethod
     def from_config(cls, configfile):
@@ -221,7 +222,11 @@ class ProductManager(object):
         if "skip_svd" in yconf["config"] and yconf["config"]["skip_svd"]:
             self.skip_svd = True
 
-        self.kltransforms = {}
+        if 'skip_svd_inv' in yconf['config'] and yconf['config']['skip_svd_inv']:
+            self.skip_svd_inv = True
+
+        self.kltransforms  = {}
+
 
         if "kltransform" in yconf:
 
@@ -268,7 +273,7 @@ class ProductManager(object):
     def generate(self):
 
         if self.gen_beams:
-            self.beamtransfer.generate(skip_svd=self.skip_svd)
+            self.beamtransfer.generate(skip_svd=self.skip_svd, skip_svd_inv=self.skip_svd_inv)
 
         if self.gen_kl:
 

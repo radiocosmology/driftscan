5b8807483cc89c98d285f86b27f3553c4226d462
Carolin Hofer <cahofer@cedar5.cedar.computecanada.ca>
2020-04-01 19:08:25 -0700

    feat(PSMonteCarloXLarge): Fisher matrix via Monte-Carlo simulations for large telescopes

--- drift/core/psmc.py
+++ drift/core/psmc.py
@@ -209,6 +209,269 @@ class PSMonteCarloAlt(psestimation.PSEstimation):
         return fisher, bias
 
 
+class PSMonteCarloXLarge(psestimation.PSEstimation, PSMonteCarlo):
+    """An extension of the PSEstimation class to support estimation of the
+    Fisher matrix via Monte-Carlo simulations for very large arrays.
+
+    Attributes
+    ----------
+    nsamples : integer
+        The number of samples to draw from each band.
+    """
+
+    nsamples = config.Property(proptype=int, default=500)
+
+    # ==== Calculate the total Fisher matrix/bias =======
+
+    def make_clzz_array(self):
+        """Store clarray for power spectrum bands in parallel with MPIArray."""
+
+        nbands = self.nbands
+        nfreq = self.telescope.nfreq
+        lmax = self.telescope.lmax
+
+        self.clarray = mpiarray.MPIArray(
+            (nbands, lmax + 1, nfreq, nfreq),
+            axis=0,
+            dtype=np.float64,
+            comm=MPI.COMM_WORLD,
+        )
+
+        self.clarray[:] = 0.0
+
+        for bl, bg in self.clarray.enumerate(axis=0):
+            self.clarray[bl] = self.make_clzz(self.band_pk[bg])
+
+    def generate(self, regen=False):
+        """Calculate the total Fisher matrix and bias and save to a file.
+
+        Parameters
+        ----------
+        regen : boolean, optional
+            Force regeneration if products already exist (default `False`).
+        """
+
+        if mpiutil.rank0:
+            st = time.time()
+            print("======== Starting PS calculation ========")
+
+        ffile = self.psdir + "/fisher.hdf5"
+
+        if os.path.exists(ffile) and not regen:
+            print("Fisher matrix file: %s exists. Skipping..." % ffile)
+            return
+
+        mpiutil.barrier()
+
+        # Pre-compute all the angular power spectra for the bands.
+        self.genbands()
+
+        comm = MPI.COMM_WORLD
+        size = comm.Get_size()
+        rank = comm.Get_rank()
+
+        st = time.time()
+
+        m_chunks, low_bound, upp_bound = self.split_single(
+            (self.telescope.mmax + 1), size
+        )
+        num_chunks = m_chunks.shape[0]
+
+        if mpiutil.rank0:
+            print(
+                "M chunks, lower bounds, upper bounds", m_chunks, low_bound, upp_bound
+            )
+
+        n = self.nsamples
+
+        # This is designed such that at each iteration one rank gets one m-mode.
+        for ci, num_m in enumerate(m_chunks):
+            if mpiutil.rank0:
+                print("Starting chunk %i of %i" % (ci, num_chunks))
+
+            loc_num, loc_start, loc_end = mpiutil.split_local(num_m)
+            mi = np.arange(low_bound[ci], upp_bound[ci])[loc_start:loc_end]
+
+            # At last iteration of m's, it is necessary to assign m's to remaining ranks otherwise the MPI communication crashes.
+            if len(mi) != 0:
+                print("Processing m-mode %i on rank %i" % (mi, rank))
+
+            if len(mi) == 0:
+                mi = np.array([low_bound[ci] + rank])
+
+            nfreq = self.telescope.nfreq
+            lmax = self.telescope.lmax
+
+            if loc_num > 0:
+                if self.num_evals(mi) > 0:
+                    # Generate random KL data
+                    x = self.gen_sample(mi, n)
+                    vec1, vec2 = self.project_vector_kl_to_sky(mi, x)
+                    # Make array contiguous
+                    vec1 = np.ascontiguousarray(
+                        vec1.reshape(loc_num, nfreq, lmax + 1, n)
+                    )
+                    vec2 = np.ascontiguousarray(
+                        vec2.reshape(loc_num, nfreq, lmax + 1, n)
+                    )
+
+                # If I don't have evals - return zero vector
+                else:
+                    vec1 = np.zeros((loc_num, nfreq, lmax + 1, n), dtype=np.complex128)
+                    vec2 = vec1
+
+            else:
+                vec1 = np.zeros((1, nfreq, lmax + 1, n), dtype=np.complex128)
+                vec2 = vec1
+
+            self.qa = mpiarray.MPIArray(
+                (self.telescope.mmax + 1, self.nbands, n),
+                axis=1,
+                comm=MPI.COMM_WORLD,
+                dtype=np.float64,
+            )
+
+            self.qa[:] = 0.0
+
+            et = time.time()
+
+            dsize = np.prod(vec1.shape)
+
+            for ir in range(size):
+                st_ir = time.time()
+                # Only fill qa if we haven't reached mmax
+                if mi < (self.telescope.mmax + 1):
+                    self.q_estimator(mi, vec1, vec2)
+
+                etq = time.time()
+                print("Time needed for calculating qa one round", etq - st_ir)
+
+                # We do the MPI communications only (size - 1) times
+                if ir == (size - 1):
+                    break
+
+                recv_buffer = self.recv_send_data(vec1, axis=-1)
+                vec1 = recv_buffer
+
+                # Sent data to (rank + 1) % size, hence local mi must be updated to (mi -1) % size
+                mi = low_bound[ci] + (mi - 1) % size
+
+            etallq = time.time()
+            print("Time needed for qa calculation all ranks ", etallq - et)
+
+        em = time.time()
+        print("Time needed for qa calculation all m chunks ", em - et)
+
+        # Once done with all the m's, redistribute qa array over m's
+        self.qa = self.qa.redistribute(axis=0)
+
+        # Make an array for local fisher an bias
+        fisher_loc = np.zeros((self.nbands, self.nbands), dtype=np.float64)
+        bias_loc = np.zeros((self.nbands,), dtype=np.float64)
+
+        # Calculate fisher for each m
+        for ml, mg in self.qa.enumerate(axis=0):
+            fisher_m = np.cov(self.qa[ml])
+            bias_m = np.mean(self.qa[ml], axis=1)
+            # Sum over all local m-modes to get the overall fisher and bias per parallel process
+            fisher_loc += fisher_m.real  # be careful with the real
+            bias_loc += bias_m.real
+
+        self.fisher = mpiutil.allreduce(fisher_loc, op=MPI.SUM)
+        self.bias = mpiutil.allreduce(bias_loc, op=MPI.SUM)
+
+        self.write_fisher_file()
+
+    def q_estimator(self, mi, vec1, vec2, noise=False):
+        """Calculate the quadratic estimator for this mi with data vec1 and vec2"""
+        lside = self.telescope.lmax + 1
+
+        for bi, bg in self.qa.enumerate(axis=1):
+            for li in range(lside):
+                lxvec = vec1[:, :, li]
+                lyvec = vec2[:, :, li]
+                self.qa[mi, bi, :] += np.sum(
+                    lyvec.conj()
+                    * np.matmul(self.clarray[bi][li].astype(np.complex128), lxvec),
+                    axis=1,
+                ).astype(np.float64)
+
+        # Calculate q_a for noise power (x0^H N x0 = |x0|^2)
+        if noise:
+            # If calculating crosspower don't include instrumental noise
+            noisemodes = 0.0 if self.crosspower else 1.0
+            evals, evecs = self.kltrans.modes_m(mi)
+            noisemodes = noisemodes + (evals if self.zero_mean else 0.0)
+
+            qa.global_slice[mi, -1] = np.sum(
+                (vec1 * vec2.conj()).T.real * noisemodes, axis=-1
+            )
+
+    def recv_send_data(self, data, axis):
+
+        comm = MPI.COMM_WORLD
+        size = comm.Get_size()
+        rank = comm.Get_rank()
+
+        shape = data.shape
+        dtype = data.dtype
+
+        recv_rank = (rank - 1) % size
+        send_rank = (rank + 1) % size
+
+        recv_buffer = np.zeros(shape, dtype=dtype)
+
+        # Need to send in 4GB chunks due to some MPI library.
+        message_size = 4 * 2 ** 30.0
+        dsize = np.prod(shape) * 16.0
+        num_messages = int(np.ceil(dsize / message_size))
+        print("Number of messages: %i" % num_messages)
+        # If dsize =0.0 you get 0 num_messages and it throws error. hack.
+        if num_messages == 0:
+            num_messages = 1
+
+        num, sm, em = mpiutil.split_m(shape[axis], num_messages)
+
+        for i in range(num_messages):
+            slc = [slice(None)] * len(shape)
+            slc[axis] = slice(sm[i], em[i])
+
+            di = np.ascontiguousarray(data[slc], dtype=dtype)
+            bi = np.zeros(di.shape, dtype=dtype)
+
+            # Initiate non-blocking receive
+            request = comm.Irecv(
+                [bi, MPI.DOUBLE_COMPLEX], source=recv_rank, tag=(i * size) + recv_rank
+            )
+            # Initiate send
+            comm.Send([di, MPI.DOUBLE_COMPLEX], dest=send_rank, tag=(i * size) + rank)
+            # Wait for receive
+            request.Wait()
+
+            # Fill recv buffer with messages
+            recv_buffer[slc] = bi
+
+        return recv_buffer
+
+    def split_single(self, m, size):
+
+        quotient = m // size
+        rem = m % size
+
+        if rem != 0:
+            m_chunks = np.append((size * np.ones(quotient, dtype=int)), rem)
+        else:
+            m_chunks = size * np.ones(quotient, dtype=int)
+
+        bound = np.cumsum(np.insert(m_chunks, 0, 0))
+        ms = m_chunks.shape[0]
+
+        low_bound = bound[:ms]
+        upp_bound = bound[1 : (ms + 1)]
+
+        return m_chunks, low_bound, upp_bound
+
+
 def sim_skyvec(trans, n):
     """Simulate a set of alm(\nu)'s for a given m.
 

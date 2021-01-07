13a09c166dcbdc99c75370cf213a9a85235ddb92
Rick Nitsche <rick@phas.ubc.ca>
2021-01-07 11:06:51 -0800

    refactor: increase minimum version to Python 3.6
    BREAKING CHANGE: Python 2 support has been removed

--- drift/core/manager.py
+++ drift/core/manager.py
@@ -1,11 +1,4 @@
-"""Manage access to and generation of driftscan analysis products.
-"""
-# === Start Python 2/3 compatibility
-from __future__ import absolute_import, division, print_function, unicode_literals
-from future.builtins import *  # noqa  pylint: disable=W0401, W0614
-from future.builtins.disabled import *  # noqa  pylint: disable=W0401, W0614
-
-# === End Python 2/3 compatibility
+"""Manage access to and generation of driftscan analysis products."""
 
 import os.path
 import shutil

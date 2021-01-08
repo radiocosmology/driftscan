92ec93e351243cd9c618c40121133dc577b9e118
Rick Nitsche <rick@phas.ubc.ca>
2021-01-08 10:45:12 -0800

    refactor: increase minimum version to Python 3.6
    BREAKING CHANGE: Python 2 support has been removed

--- doc/conf.py
+++ doc/conf.py
@@ -11,14 +11,7 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-# === Start Python 2/3 compatibility
-from __future__ import absolute_import, division, print_function, unicode_literals
-from future.builtins import *  # noqa  pylint: disable=W0401, W0614
-from future.builtins.disabled import *  # noqa  pylint: disable=W0401, W0614
-
-# === End Python 2/3 compatibility
-
-import sys, os
+import os
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
--- drift/core/telescope.py
+++ drift/core/telescope.py
@@ -8,8 +8,6 @@ from caput import time as ctime
 from cora.util import hputil, units
 
 from drift.core import visibility
-from drift.util import util
-from future.utils import with_metaclass
 
 
 def in_range(arr, min, max):
@@ -118,7 +116,7 @@ def max_lm(baselines, wavelengths, uwidth, vwidth=0.0):
     return lmax, mmax
 
 
-class TransitTelescope(with_metaclass(abc.ABCMeta, config.Reader, ctime.Observer)):
+class TransitTelescope(config.Reader, ctime.Observer, metaclass=abc.ABCMeta):
     """Base class for simulating any transit interferometer.
 
     This is an abstract class, and several methods must be implemented before it
@@ -898,7 +896,7 @@ class TransitTelescope(with_metaclass(abc.ABCMeta, config.Reader, ctime.Observer
     # ===================================================
 
 
-class UnpolarisedTelescope(with_metaclass(abc.ABCMeta, TransitTelescope)):
+class UnpolarisedTelescope(TransitTelescope, metaclass=abc.ABCMeta):
     """A base for an unpolarised telescope.
 
     Again, an abstract class, but the only things that require implementing are
@@ -998,7 +996,7 @@ class UnpolarisedTelescope(with_metaclass(abc.ABCMeta, TransitTelescope)):
         return bnoise[..., np.newaxis] * 0.5  # Correction for unpolarisedness
 
 
-class PolarisedTelescope(with_metaclass(abc.ABCMeta, TransitTelescope)):
+class PolarisedTelescope(TransitTelescope, metaclass=abc.ABCMeta):
     """A base for a polarised telescope.
 
     Again, an abstract class, but the only things that require implementing
@@ -1081,7 +1079,7 @@ class PolarisedTelescope(with_metaclass(abc.ABCMeta, TransitTelescope)):
     # ===================================================
 
 
-class SimpleUnpolarisedTelescope(with_metaclass(abc.ABCMeta, UnpolarisedTelescope)):
+class SimpleUnpolarisedTelescope(UnpolarisedTelescope, metaclass=abc.ABCMeta):
     """A base for a polarised telescope.
 
     Again, an abstract class, but the only things that require implementing are
@@ -1108,7 +1106,7 @@ class SimpleUnpolarisedTelescope(with_metaclass(abc.ABCMeta, UnpolarisedTelescop
         return self._single_feedpositions
 
 
-class SimplePolarisedTelescope(with_metaclass(abc.ABCMeta, PolarisedTelescope)):
+class SimplePolarisedTelescope(PolarisedTelescope, metaclass=abc.ABCMeta):
     """A base for a polarised telescope.
 
     Again, an abstract class, but the only things that require implementing are

178b598b7c8c0a43471c48a12bd7c3e316a6d6c7
Richard Shaw <richard@phas.ubc.ca>
2021-07-06 16:05:19 -0700

    test(test_functional_skip): test freq by freq loading of BTMs

--- tests/test_functional_skip.py
+++ tests/test_functional_skip.py
@@ -42,3 +42,27 @@ def test_skip_beam_m(manager, saved_products):
 
     # Test that the entries expected to be missing are indeed missing
     assert bm == approx(bm_saved)
+
+
+def test_skip_beam_m_iter(manager, saved_products):
+    """Check the consistency of the m-ordered beams."""
+
+    with saved_products("beam_m_14.hdf5") as f:
+        bm_saved = f["beam_m"][:]
+
+    # Apply the masking from the skipped entries to the saved data
+    skipped_freq = manager.config["telescope"]["skip_freq"]
+    bm_saved[skipped_freq] = 0.0
+    skipped_baselines = manager.config["telescope"]["skip_baselines"]
+    bm_saved[:, :, skipped_baselines] = 0.0
+    if manager.config["telescope"]["skip_pol"]:
+        bm_saved[:, :, :, 1:] = 0.0
+
+    for fi in range(manager.telescope.nfreq):
+        bm = manager.beamtransfer.beam_m(14, fi=fi)
+
+        # Check the shapes are consistent
+        assert bm_saved[fi].shape == bm.shape
+
+        # Test that the entries expected to be missing are indeed missing
+        assert bm == approx(bm_saved[fi])

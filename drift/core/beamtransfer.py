fix(BeamTransfer): fix bug in baseline masking of noise power
Also add a few comments about noise prewhitening

drift/core/beamtransfer.py
@@ -843,8 +843,9 @@ class BeamTransfer(config.Reader):
                             self.telescope.lmax + 1,
                         )
 
+                    # Prewhiten beam transfer matrix by multiplying by N^-1/2 matrix
                     noisew = self.telescope.noisepower(
-                        np.arange(self.telescope.npairs[bl_mask]), fi
+                        np.arange(self.telescope.npairs)[bl_mask], fi
                     ).flatten() ** (-0.5)
                     noisew = np.concatenate([noisew, noisew])
                     bf = bf * noisew[:, np.newaxis, np.newaxis]
@@ -914,7 +915,10 @@ class BeamTransfer(config.Reader):
                         beam = np.dot(ut3, bfr)
 
                         # Save out the evecs (for transforming from the telescope frame
-                        # into the SVD basis)
+                        # into the SVD basis). We multiply ut by N^{-1/2} because ut
+                        # must acts on N^{-1/2} v, not v alone (where v are the
+                        # visibilities), so we include that factor of N^{-1/2} in
+                        # dset_ut so that we can apply it directly to v in the future.
                         dset_ut[fi, :nmodes] = ut * noisew[np.newaxis, :]
 
                         # Save out the modified beam matrix (for mapping from the sky